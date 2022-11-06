a2485c01175372b7875b25ea4e71f6c0d571c218
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-11-06 00:13:27 -0500

    Organize repo to fit more conventional python standards

---
+++ main.py
@@ -0,0 +1,44 @@
+from grayscale import Grayscale
+from time import time
+from window_capture import WindowCapture
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def main():
+	# initialize the WindowCapture class
+	trials = WindowCapture('Trials Fusion')
+
+	while(True):
+		loop_time = time()
+		# Get an updated image of the game every frame
+		screenshot = trials.get_screenshot()	
+		# grayscale = Grayscale()
+		# grayscale.find_marker(screenshot)
+
+		# Convert stream of images (video) into grayscale one by one
+		grayImg = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
+		ret, thresh = cv.threshold(grayImg, 127, 255, 0)
+
+		# Find & Draw contours of image
+		contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		cv.drawContours(screenshot, contours, -1, (0,255,0), 3)
+	
+		# Display stream of images with contours
+		cv.imshow('Computer Vision', screenshot)
+
+		# Debug the loop rate
+		print('FPS {}'.format(1 / (time() - loop_time)))
+		
+		# press 'q' with the output window focused to exit.
+		# waits 1 ms every loop to process key presses
+		if cv.waitKey(1) == ord('q'):
+			cv.destroyAllWindows()
+			break
+
+	print('Done.')
+
+if __name__ == "__main__":
+    main()
